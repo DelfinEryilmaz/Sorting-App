d436cdad5530a95783e98e6284a1c242631a76c1
Delfin Eryılmaz <deryilmaz06@gmail.com>
2026-01-03 16:17:33 +0300

    Implement all merge sort methods with 2 parts

--- sorting_app/src/main/java/com/delfin/SortingAlgorithms/MergeSort.java
+++ sorting_app/src/main/java/com/delfin/SortingAlgorithms/MergeSort.java
@@ -10,43 +10,149 @@ public class MergeSort implements VisualizableAlgorithm{
      * @param arr
      * @param isTwoPart
      */
-    public static void mergeSort(int[] arr, boolean isTwoPart) {
+    public static void mergeSort(int[] arr, boolean isTwoPart, boolean inPlace) {
         if (isTwoPart) {
-            int partLength = arr.length / 2;
-            sortInPlace(0, partLength, arr);
-            sortInPlace(partLength + 1, arr.length - 1, arr);
+            if (inPlace) {
+                sortInPlace2(0, arr.length - 1, arr);
+            } else {
+                int[] temp = new int[arr.length];
+                sort2(0, arr.length - 1, arr, temp);
+            }
         } else {
 
         }
     }
 
-    public static void sort() {
-
-    }
-
-    public static void sortInPlace(int start, int finish, int[] arr) {
+    /**
+     * Recursive helper method for sorting with a temp array.
+     * @param start
+     * @param finish
+     * @param arr
+     * @param tempArr
+     */
+    private static void sort2(int start, int finish, int[] arr, int[] tempArr) {
         if (finish - start == 1) {
             if (arr[start] > arr[finish]) {
                 int temp = arr[finish];
                 arr[finish] = arr[start];
                 arr[start] = temp;
-                printArray(arr);
                 return;
             }
-
+                
         } else if (finish != start) {
             int mid = (start + finish) / 2;
-            sortInPlace(start, mid - 1, arr);
-            sortInPlace(mid, finish, arr);
-            mergeTwo(arr, start, mid);
+            sort2(start, mid, arr, tempArr);
+            sort2(mid + 1, finish, arr, tempArr);
+            merge2(arr, tempArr, start, mid, finish);
+        }
+        // If the remaining part is one element which is start == finish
+        return;
+    }
+
+    /**
+     * Helper merge method using a temp array.
+     * @param arr
+     * @param temp
+     * @param start
+     * @param mid
+     * @param finish
+     */
+    private static void merge2(int[] arr, int[] temp, int start, int mid, int finish) {
+        int i = start;
+        int j = mid + 1;
+        int k = start;
+
+        // This loop will continue until one of the part is finish
+        while (i <= mid && j <= finish) {
+            if (arr[i] > arr[j]) {
+                temp[k] = arr[j];
+                k++;
+                j++;
+            } else {
+                temp[k] = arr[i];
+                k++;
+                i++;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }
+
+        while (j <= finish) {
+            temp[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        // Copy the temp into original array
+        for (int l = start; l <= finish; l++) {
+            arr[l] = temp[l];
         }
     }
 
-    public static void mergeTwo(int[] arr, int start, int mid) {
-        int i = start;
-        int j = mid;
-        while (start <= mid) {
+    /**
+     * Recursive helper method for sorting in-place.
+     * @param start
+     * @param finish
+     * @param arr
+     */
+    private static void sortInPlace2(int start, int finish, int[] arr) {
+        if (start >= finish) return;
+        if (finish - start == 1) {
+            if (arr[start] > arr[finish]) {
+                int temp = arr[finish];
+                arr[finish] = arr[start];
+                arr[start] = temp;
+                return;
+            }
+        } else if (finish != start) {
+            int mid = (start + finish) / 2;
+            sortInPlace2(start, mid, arr);
+            sortInPlace2(mid + 1, finish, arr);
+            mergeInPlace2(arr, start, mid, finish);
+        }
+        // If the remaining part is one element which is start == finish
+        return;
+    }
 
+    /**
+     * Helper in-place merge method using array operations like shifting.
+     * @param arr
+     * @param start
+     * @param mid
+     * @param finish
+     */
+    private static void mergeInPlace2(int[] arr, int start, int mid, int finish) {
+        int start2 = mid + 1;
+
+        // If there is a direct merge
+        if (arr[mid] <= arr[start2]) {
+            return;
+        }
+
+        while (start <= mid && start2 <= finish) {
+            // If first element is at the correct place
+            if (arr[start] <= arr[start2]) {
+                start++;
+            } else {
+                int value = arr[start2];
+                int index = start2;
+
+                // Shifting all the elements
+                while (index != start) {
+                    arr[index] = arr[index - 1];
+                    index--;
+                }
+                arr[index] = value;
+
+                // Updating all pointers
+                start++;
+                mid++;
+                start2++;
+            }
         }
     }
 
@@ -54,12 +160,6 @@ public class MergeSort implements VisualizableAlgorithm{
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {12, 32, 32, 50, 5, 60, 54};
-
-        MergeSort.mergeSort(arr, true);
     }
 
     @Override
